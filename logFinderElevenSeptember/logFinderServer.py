dbf324d6ffb5f77b32adf83793c60b6b4ad54371
stepanrtWin <sjcam228@gmail.com>
2024-11-03 21:21:58 +0300

    add server to git

--- logFinderElevenSeptember/logFinderServer.py
+++ logFinderElevenSeptember/logFinderServer.py
@@ -2,6 +2,7 @@ import datetime
 
 import re
 import requests
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -11,6 +12,19 @@ app = Flask(__name__)
 CORS(app)
 list_checker = []
 nicks = []
+
+
+@app.route('/writePosPlayer', methods=['POST'])
+def write_data():
+    response = request.json
+    pos_player = response.get('data')
+    print(pos_player)
+    with open('../AnB/logsPosPlayer.txt', 'a', encoding='utf-8') as f:
+        f.write(f"{pos_player}\n")
+
+    return jsonify({'message': 'success'}), 200
+
+
 
 
 @app.route('/getLogs', methods=['POST'])
@@ -27,19 +41,25 @@ def send_data():
     if data_response.get('check_commands'):
         list_checker.append('/')
 
+    if data_response.get('check_discipline_commands'):
+        list_checker.append('/warn ')
+        list_checker.append('/mute ')
+        list_checker.append('/tempmute ')
+        list_checker.append('/kick ')
+        list_checker.append('/tempban ')
+        list_checker.append('/ban ')
+
     if data_response.get('check_pm'):
         list_checker.append('/m ')
         list_checker.append('/r ')
         list_checker.append('/w ')
         list_checker.append('/msg ')
+
     if data_response.get('nicknames') != '':
         nicks = data_response.get('nicknames').split(',')
 
     if not list_checker and not nicks:
         return jsonify({'logs': downloadLogs(date=data_response.get('first_date'))}), 200
-
-
-
     return jsonify({'logs': check_logs(data_response.get('first_date'))}), 200
 
 
@@ -77,6 +97,7 @@ def check_logs(data):
             else:
                 return o
 
+
 def check_nicks(data_logs):
     global nicks
     list_m = []
@@ -95,6 +116,7 @@ def check_nicks(data_logs):
             o += f"{c}\n"
 
         return o
+
 
 def downloadLogs(date):
     day = date.split('-')[2]
